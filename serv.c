74c64f0e36397afa6ca84fa0f2db310173b95760
kahono0 <kahonojackson@gmail.com>
2023-05-31 16:26:03 +0300

    Structizz

--- serv.c
+++ serv.c
@@ -3,16 +3,28 @@
 #include <string.h>
 #include <sys/socket.h>
 #include <unistd.h> // for write()
+#include <stdlib.h>
+#include <string.h>
 
-#define PORT 4000
 
-
+#define MAX_HEADER_LENGTH 1000
 //define request struct
+typedef struct header {
+    char* key;
+    char* value;
+} Header;
+
+typedef struct status {
+    char* method;
+    char* path;
+    char* version;
+} Status;
+
 typedef struct request{
     char* method;
     char* path;
     char* version;
-    char* headers;
+    Header headers[MAX_HEADER_LENGTH];
     char* body;
 } Request;
 
@@ -20,40 +32,88 @@ typedef struct request{
 typedef struct response{
     char* version;
     char* status;
-    char* headers;
+    Header* headers;
     char* body;
 } response;
+
+Header parseHeader(const char* string) {
+  Header header;
+
+  // Find the first occurrence of the colon ':' in the string.
+  size_t colon_index = strcspn(string, ":");
+
+  // Copy the substring up to the colon into the header's key.
+  header.key = malloc(colon_index + 1);
+  strncpy(header.key, string, colon_index);
+//   header.key[colon_index] = '\0';
+
+  // Copy the substring after the colon into the header's value.
+  header.value = malloc(strlen(string) - colon_index + 1);
+  strncpy(header.value, string + colon_index + 1, strlen(string) - colon_index);
+//   header.value[strlen(string) - colon_index] = '\0';
+
+  return header;
+}
+
+Status parseStatus(char* string) {
+  Status status;
+
+  // Split GET / HTTP/1.1
+    char* token = strtok(string, " ");
+    status.method = token;
+    token = strtok(NULL, " ");
+    status.path = token;
+    token = strtok(NULL, " ");
+    status.version = token;
+
+    return status;
+}
 
 //parse string request into request struct
 Request parseRequest(char* request){
     Request r;
-    char* token = strtok(request, " ");
-    r.method = token;
-    token = strtok(NULL, " ");
-    r.path = token;
-    token = strtok(NULL, " ");
-    r.version = token;
-    token = strtok(NULL, " ");
-    r.headers = token;
-    token = strtok(NULL, " ");
-    r.body = token;
+    int i = 0;
+    char lines[100][1000];
+    char token[1000];
+    int lineCount = 0;
+    int headerCount = 0;
+    while(request[i] != '\0'){
+        if(request[i] == '\n'){
+            strcpy(lines[lineCount], token);
+            lineCount++;
+            if(lineCount != 1){
+                Header h = parseHeader(token);
+                r.headers[lineCount] = h;
+            }else{
+                printf("first line: %s\n", token);
+                Status s = parseStatus(token);
+                r.method = s.method;
+                r.path = s.path;
+                r.version = s.version;
+
+            }
+            strcpy(token, "");
+
+        }else{
+            strncat(token, &request[i], 1);
+        }
+        i++;
+    }
+
     return r;
 }
-
-
-char* simple_tokenizer(char* s){
-    char* token = strtok(s, " ");
-    return token;
-}
-
 
 void handle(int new_client){
     char request[1000];
     read(new_client, request, 1000);
-    printf("got request: %s\n", request);
+    // printf("got request: %s\n", request);z
     Request r = parseRequest(request);
 
-    printf("headers: %s\n", r.headers);
+    printf("Method, version and path\n");
+    printf("%s\n", r.method);
+    printf("%s\n", r.version);
+    printf("%s\n", r.path);
+
 
 
  // construct a response
@@ -68,18 +128,22 @@ void handle(int new_client){
     // send the response to the client
     write(new_client, response, strlen(response));
 
-    printf("sent response:\n%s\n", response);
+    // close the connections
+    close(new_client);
+
 }
 
-int main() {
-  printf("Running Server on localhost:%d \n", PORT);
+int main(char argc, char *argv[]) {
+    //to int
+    int PORT = atoi(argv[1]);
+  printf("Running Server on http://localhost:%d \n", PORT);
 
   //printf("%s", simple_tokenizer("GET / HTTP/1.1"));
 
   // create a socket
   int the_socket = socket(AF_INET, SOCK_STREAM, 0);
   if (the_socket < 0) {
-    printf("could not create socket 😭");
+    printf("socket: %d\n", the_socket);
     return 1;
   }
 
@@ -89,7 +153,7 @@ int main() {
   addr.sin_addr.s_addr = INADDR_ANY;
   addr.sin_port = htons(PORT); // port 4000
   if (bind(the_socket, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
-    printf("could not bind socket 😭");
+    printf("could not bind socket \n");
     return 1;
   }
 
@@ -100,16 +164,19 @@ int main() {
     // accept connections
     int new_client = accept(the_socket, NULL, NULL);
     if (new_client < 0) {
-      printf("could not accept connection 😭");
+      printf("could not accept connection\n");
       return 1;
     }
 
     //read request
 
     // connected to a client!
-    printf("connected to client 🥳\n");
+    printf("connected to client \n");
     handle(new_client);
-}
+  }
+
+  // close the the_socket
+  close(the_socket);
 
   return 0;
 }
--- server.c
+++ server.c
@@ -1,49 +1,226 @@
-#include<stdio.h>
+// /*
+// #include<stdio.h>
+// #include <string.h>
+
+// typedef struct request{
+//     char* method;
+//     char* path;
+//     char* version;
+//     char* headers;
+//     char* body;
+// } Request;
+
+// int main() {                              //
+//    FILE *fp = fopen("req.txt", "r");
+//    if (fp == NULL) {
+//       printf("Error opening file\n");
+//       return 1;
+//    }
+
+//    //read lines
+
+//    char line[1000];
+
+//    fgets(line, sizeof(line), fp);
+//    Request r;
+//    char* token = strtok(line, " ");
+//    printf("method: %s\n", r.method);
+//    //headers
+//    char* headers[50];
+
+//    int i = 0;
+//    while (fgets(line, sizeof(line), fp)) {
+//       //printf("%s", line);
+//       headers[i] = line;
+//       i++;
+//    }
+//    printf("%s\n", r.method);
+//    if(strcmp(r.method, "GET") != 0){
+//        //remove body from headers
+//        r.body = headers[i-1];
+//    }else{
+//        printf("no body\n");
+//    }
+
+//    return 0;
+// }*/
+
+// #include <stdio.h>
+// #include <string.h>
+
+// #define MAX_PAIRS 2
+// #define MAX_KEY_LENGTH 100
+// #define MAX_VALUE_LENGTH 100
+
+// int main() {
+//     char input[] = "hello:world\nmy:name";
+
+//     char pairs[MAX_PAIRS][2][MAX_VALUE_LENGTH];
+
+//     // Tokenize the input string
+//     char* token;
+//     int row = 0;
+//     int col = 0;
+
+//     token = strtok(input, "\n:");
+//     while (token != NULL) {
+//         strcpy(pairs[row][col], token);
+
+//         col++;
+//         if (col == 2) {
+//             col = 0;
+//             row++;
+//         }
+
+//         token = strtok(NULL, "\n:");
+//     }
+
+//     // Print the two-dimensional array
+//     for (int i = 0; i < MAX_PAIRS; i++) {
+//         printf("%s, %s\n", pairs[i][0], pairs[i][1]);
+//     }
+
+//     return 0;
+// }
+
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <string.h>
+
+// typedef struct {
+//     char* key;
+//     char* value;
+// } Header;
+
+// typedef struct {
+//     char* method;
+//     char* path;
+//     char* version;
+//     Header* headers;
+//     int num_headers;
+// } Request;
+
+// Request parseHttpRequest(const char* requestString) {
+//     Request request;
+
+//     char* mutableRequestString = strdup(requestString);
+//     char* line = strtok(mutableRequestString, "\r\n");
+
+//     // Parse the first line (method, path, version)
+//     char* token = strtok(line, " ");
+//     request.method = strdup(token);
+
+//     token = strtok(NULL, " ");
+//     request.path = strdup(token);
+
+//     token = strtok(NULL, " ");
+//     request.version = strdup(token);
+
+//     // Parse the headers
+//     request.headers = NULL;
+//     request.num_headers = 0;
+
+//     while ((line = strtok(NULL, "\r\n")) != NULL) {
+//         Header header;
+
+//         char* colon = strchr(line, ':');
+//         if (colon != NULL) {
+//             *colon = '\0';
+//             header.key = strdup(line);
+//             header.value = strdup(colon + 2); // Skip the space after the colon
+
+//             request.headers = realloc(request.headers, (request.num_headers + 1) * sizeof(Header));
+//             request.headers[request.num_headers++] = header;
+//         }
+//     }
+
+//     free(mutableRequestString);
+
+//     return request;
+// }
+
+// int main() {
+//     const char* requestString = "GET / HTTP/1.1\r\n"
+//                                 "Host: localhost:4000\r\n"
+//                                 "Connection: keep-alive\r\n"
+//                                 "Cache-Control: max-age=0\r\n"
+//                                 "sec-ch-ua: \"Google Chrome\";v=\"113\", \"Chromium\";v=\"113\", \"Not-A.Brand\";v=\"24\"\r\n"
+//                                 "sec-ch-ua-mobile: ?0\r\n"
+//                                 "sec-ch-ua-platform: \"Linux\"\r\n"
+//                                 "Upgrade-Insecure-Requests: 1\r\n"
+//                                 "User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36\r\n"
+//                                 "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7\r\n"
+//                                 "Sec-Fetch-Site: none\r\n"
+//                                 "Sec-Fetch-Mode: navigate\r\n"
+//                                 "Sec-Fetch-User: ?1\r\n"
+//                                 "Sec-Fetch-Dest: document\r\n"
+//                                 "Accept-Encoding: gzip, deflate, br\r\n"
+//                                 "Accept-Language: en-US,en-KE;q=0.9,en;q=0.8\r\n"
+//                                 "Cookie: _smart_farm_key=SFMyNTY.g3QAAAACbQAAAAtfY3NyZl90b2tlbm0AAAAYaHVZckhESTBfYzNpckU5WG5uOS1zeC02bQAAAAd1c2VyX2lkbQAAACQ4ZWEwOTljYy0xOTdjLTQ2ZDAtODE4Zi0yZWNmMTgyOGM0MmQ.KvFvmeyrOij5sUB3LBKzjkoIvlFuDCVovP8t0hWzoO0\r\n";
+
+//     Request request = parseHttpRequest(requestString);
+
+//     printf("Method: %s\n", request.method);
+//     printf("Path: %s\n", request.path);
+//     printf("Version: %s\n", request.version);
+
+//     for (int i = 0; i < request.num_headers; i++) {
+//         printf("Header: %s - %s\n", request.headers[i].key, request.headers[i].value);
+//     }
+
+//     free(request.method);
+//     free(request.path);
+//     free(request.version);
+//     for (int i = 0; i < request.num_headers; i++) {
+//         free(request.headers[i].key);
+//         free(request.headers[i].value);
+//     }
+//     free(request.headers);
+
+//     return 0;
+// }
+
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
-typedef struct request{
-    char* method;
-    char* path;
-    char* version;
-    char* headers;
-    char* body;
-} Request;
+typedef struct header {
+    char* key;
+    char* value;
+} Header;
 
-int main() {                              //
-   FILE *fp = fopen("req.txt", "r");
-   if (fp == NULL) {
-      printf("Error opening file\n");
-      return 1;
-   }
+Header split_header(const char* string) {
+  Header header;
 
-   //read lines
+  // Find the first occurrence of the colon ':' in the string.
+  size_t colon_index = strcspn(string, ":");
 
-   char line[1000];
+  // Copy the substring up to the colon into the header's key.
+  header.key = malloc(colon_index + 1);
+  strncpy(header.key, string, colon_index);
+  header.key[colon_index] = '\0';
 
-   fgets(line, sizeof(line), fp);
-   Request r;
+  // Copy the substring after the colon into the header's value.
+  header.value = malloc(strlen(string) - colon_index + 1);
+  strncpy(header.value, string + colon_index + 1, strlen(string) - colon_index);
+  header.value[strlen(string) - colon_index] = '\0';
 
-   r.method = strtok(line, " ");
-   r.path = strtok(NULL, " ");
-   r.version = strtok(NULL, " ");
-   printf("method: %s\n", r.method);
-   printf("headers %s:", r.method);
-   //headers
-   char* headers[50];
+  return header;
+}
 
-   int i = 0;
-   while (fgets(line, sizeof(line), fp)) {
-      //printf("%s", line);
-      headers[i] = line;
-      i++;
-   }
 
-   if(strcmp(r.method, "GET") != 0){
-       //remove body from headers
-       r.body = headers[i-1];
-   }else{
-       printf("no body\n");
-   }
+int main() {
+    const char* input = "Host: localhost:3000";
+    Header header = split_header(input);
 
-   return 0;
+    char* string = "Host: localhost:3000";
+    printf("String: ***%s\n", string);
+
+    printf("Key: %s\n", header.key);
+    printf("Value: %s\n", header.value);
+
+    free(header.key);
+    free(header.value);
+
+    return 0;
 }
